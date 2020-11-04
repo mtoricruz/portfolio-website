d2ba34b3dacb46ebb1e0aba554abd584d170d24e
Toricruz Mendiola <mtoricruz@gmail.com>
2020-11-03 19:27:36 -0800

    added 4 projects and changed respective links

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -43,7 +43,7 @@ export default function Projects(){
                 <div className='project'>
                     <div className='project-info'>
                         <h2>Conway's Game of Life</h2>
-                        <img src={require('../styling/imgs/game-of-life.PNG')} alt='citrics-pic'/>
+                        <img src={require('../styling/imgs/game-of-life.PNG')} alt='game-of-life'/>
                     </div>
                     <div className='hover-container' style={{opacity: '0'}}>
                         <div className='hover-col-1'>
@@ -74,20 +74,20 @@ export default function Projects(){
                 </div>
                 <div className='project'>
                     <div className='project-info'>
-                        <h2>Citrics - Find the City for You</h2>
-                        <img src={require('../styling/imgs/citricsimg.PNG')} alt='citrics-pic'/>
+                        <h2>African Marketplace REST API</h2>
+                        <img src={require('../styling/imgs/african-market-place.PNG')} alt='build-week-4'/>
                     </div>
                     <div className='hover-container' style={{opacity: '0'}}>
                         <div className='hover-col-1'>
                             <a 
-                                href='https://a.citrics.dev' 
+                                href='https://documenter.getpostman.com/view/11589327/Szzq4Eeg?version=latest' 
                                 target='_blank' 
                                 rel='noopener noreferrer'
                             >
                                 <i className="fas fa-link fa-8x"/>
                             </a>
                             <a 
-                                href='https://github.com/Lambda-School-Labs/Labs26-Citrics-FE-TeamA/tree/main' 
+                                href='https://github.com/mtoricruz/back-end' 
                                 target='_blank' 
                                 rel='noopener noreferrer'
                             >
@@ -95,31 +95,31 @@ export default function Projects(){
                             </a> 
                         </div>
                         <div className='hover-col-2'> 
-                            <p>React.js</p>
-                            <p>Cypress.io</p>
-                            <p>Jest/Enzyme</p>
-                            <p>Sass</p>
-                            <p>Ant Design</p>
-                            <p>Plotly.js</p>
+                            <p>Node.js</p>
+                            <p>PostgreSQL</p>
+                            <p>JWT</p>
+                            <p>SQLiteStudio</p>
+                            <p>Jest</p>
+                            <p>bcryptjs</p>
                         </div>
                     </div>
                 </div>
                 <div className='project'>
                     <div className='project-info'>
-                        <h2>Citrics - Find the City for You</h2>
-                        <img src={require('../styling/imgs/citricsimg.PNG')} alt='citrics-pic'/>
+                        <h2>Saltiest Hacker News Trolls</h2>
+                        <img src={require('../styling/imgs/saltiest-hackers.PNG')} alt='build-week-3'/>
                     </div>
                     <div className='hover-container' style={{opacity: '0'}}>
                         <div className='hover-col-1'>
                             <a 
-                                href='https://a.citrics.dev' 
+                                href='https://nervous-colden-ddbad4.netlify.app/feed' 
                                 target='_blank' 
                                 rel='noopener noreferrer'
                             >
                                 <i className="fas fa-link fa-8x"/>
                             </a>
                             <a 
-                                href='https://github.com/Lambda-School-Labs/Labs26-Citrics-FE-TeamA/tree/main' 
+                                href='https://github.com/mtoricruz/Frontend' 
                                 target='_blank' 
                                 rel='noopener noreferrer'
                             >
@@ -128,11 +128,11 @@ export default function Projects(){
                         </div>
                         <div className='hover-col-2'> 
                             <p>React.js</p>
-                            <p>Cypress.io</p>
+                            <p>Redux</p>
                             <p>Jest/Enzyme</p>
-                            <p>Sass</p>
-                            <p>Ant Design</p>
-                            <p>Plotly.js</p>
+                            <p>Green Sock</p>
+                            <p>yup form validation</p>
+                            <p>Reactstrap</p>
                         </div>
                     </div>
                 </div>
